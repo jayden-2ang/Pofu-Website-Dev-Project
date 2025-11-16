0714d212a2cd7ebd0417a5233dd2e381202061a3
Jayden <jaydenang2@gmail.com>
2025-11-16 14:56:27 -0800

    Update nav links to php

--- photography.php
+++ photography.php
@@ -9,15 +9,15 @@
 <body>
     <header>
         <nav>
-            <h1><a href="index.html">Pofu Photography</a></h1>
+            <h1><a href="index.php">Pofu Photography</a></h1>
             <ul>
-                <li><a href="photobooth.html">Photo Booth</a></li>
-                <li><a href="backdrops.html">Backdrops</a></li>
-                <li><a href="photography.html">Photography</a></li>
+                <li><a href="photobooth.php">Photo Booth</a></li>
+                <li><a href="backdrops.php">Backdrops</a></li>
+                <li><a href="photography.php">Photography</a></li>
                 <li><a href="https://pofuphotos.smugmug.com/Client-Photos-">Gallery</a></li>
-                <li><a href="contact.html">Contact Us</a></li>
-                <li><a href="documentation.html">Documentation</a></li>
-                <li><a href="sources.html">Sources</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+                <li><a href="documentation.php">Documentation</a></li>
+                <li><a href="sources.php">Sources</a></li>
             </ul>
         </nav>
     </header>
