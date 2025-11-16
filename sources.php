3b0d96025c447624271562fd2ef2cd77983d0c2a
Jayden <jaydenang2@gmail.com>
2025-11-15 17:11:49 -0800

    Add source page

---
+++ sources.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pofu Photography</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1><a href="index.html">Pofu Photography</a></h1>
+            <ul>
+                <li><a href="photobooth.html">Photo Booth</a></li>
+                <li><a href="backdrops.html">Backdrops</a></li>
+                <li><a href="photography.html">Photography</a></li>
+                <li><a href="https://pofuphotos.smugmug.com/Client-Photos-">Gallery</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <footer>
+        <h2>Photo Booth & Event Photography</h2>
+        <ul>
+            <li><a href="mailto:keith@pofuphotography.com">keith@pofuphotography.com</a></li><br>
+            <li><a href="tel:604-799-4901">(604)799.4901</a></li><br>
+            <li><a href="contact.html"><button class="order" id="order">Contact Us!</button></a></li><br>
+            <li><p>New Westminster, BC, Canada</p></li>
+        </ul>
+    </footer>
+</body>
+</html>
