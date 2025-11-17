f6f196d5ac2a5bd81fa4677a92663d1cd964acf6
Jayden <jaydenang2@gmail.com>
2025-11-16 22:01:12 -0800

    Add contact page

---
+++ contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pofu Photography</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1><a href="index.php">Pofu Photography</a></h1>
+            <ul>
+                <li><a href="photobooth.php">Photo Booth</a></li>
+                <li><a href="backdrops.php">Backdrops</a></li>
+                <li><a href="photography.php">Photography</a></li>
+                <li><a href="https://pofuphotos.smugmug.com/Client-Photos-">Gallery</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+                <li><a href="documentation.php">Documentation</a></li>
+                <li><a href="sources.php">Sources</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <footer>
+        <h2>Photo Booth & Event Photography</h2>
+        <ul>
+            <li><a href="mailto:keith@pofuphotography.com">keith@pofuphotography.com</a></li><br>
+            <li><a href="tel:604-799-4901">(604)799.4901</a></li><br>
+            <li><a href="contact.html"><button class="order" id="order">Contact Us!</button></a></li><br>
+            <li><p>New Westminster, BC, Canada</p></li>
+        </ul>
+    </footer>
+</body>
+</html>
